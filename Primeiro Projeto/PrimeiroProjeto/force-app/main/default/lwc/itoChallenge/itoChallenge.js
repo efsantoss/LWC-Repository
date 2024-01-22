25af9e2c409f56f9d5a36ad9747e999cf11faab5
Enzo <enzo_fischer10@hotmail.com>
2024-01-21 22:02:50 -0300

    Upando o codigo do Ito

--- Primeiro Projeto/PrimeiroProjeto/force-app/main/default/lwc/itoChallenge/itoChallenge.js
+++ Primeiro Projeto/PrimeiroProjeto/force-app/main/default/lwc/itoChallenge/itoChallenge.js
@@ -1,21 +1,38 @@
 import { LightningElement, wire, track, api } from 'lwc';
-import { getContact } from 'lightning/uiContactApi';
-import ContactName from '@salesforce/schema/Contact.Name';
+import { getRecord } from 'lightning/uiRecordApi';
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import CONTACT_NAME_FIELD from '@salesforce/schema/Contact.Name';
+import CONTACT_ID_FIELD from '@salesforce/schema/Contact.Id';
 
 export default class ItoChallenge extends LightningElement {
 
-    @track createdName = ' ';
-    @track createdId = ' ';
+    @track createdName = '';
+    @track createdId = '';
 
     @api contactId;
-    @wire(getContact, { ContactName:`$contactId`, fields: Contacts })
+    @wire(getRecord, {
+        recordId: '$contactId',
+        fields: [CONTACT_NAME_FIELD, CONTACT_ID_FIELD]
+    })
     contact;
+
     get name() {
         return this.contact.data.fields.Name.value;
     }
+
     get id() {
-        return this.contact.data.fields.id.value;
+        return this.contact.data.fields.Id.value;
     }
 
+    handleInputName(event) {
+        this.createdName = event.target.value;
+    }
 
+    handleInputId(event) {
+        this.createdId = event.target.value;
+    }
+
+    handleClick() {
+        // Chame o método Apex para salvar o contato com os valores inseridos
+    }
 }
