30199b6dbe8fea9d1d1e20b1b4d520b409b2da10
Enzo <enzo_fischer10@hotmail.com>
2024-01-23 10:41:10 -0300

    Upando o codigo do Ito

--- Primeiro Projeto/PrimeiroProjeto/force-app/main/default/lwc/searchPerson/searchPerson.js
+++ Primeiro Projeto/PrimeiroProjeto/force-app/main/default/lwc/searchPerson/searchPerson.js
@@ -1,3 +1,37 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import searchContacts from '@salesforce/apex/SearchContact.searchContacts';
 
-export default class SearchPerson extends LightningElement {}
+export default class searchPerson extends LightningElement {
+    @track searchName = '';
+    @track searchLastName = '';
+    @track searchDocument = '';
+
+    get documentLabel() {
+        return 'Documento';
+    }
+
+    // Utilize o @wire para buscar contatos com base nas informações fornecidas
+    @wire(searchContacts, { firstName: '$searchName', lastName: '$searchLastName', document: '$searchDocument' })
+    contacts;
+
+    handleInputName(event) {
+        this.searchName = event.target.value;
+    }
+
+    handleInputLastName(event) {
+        this.searchLastName = event.target.value;
+    }
+
+    handleInputDocument(event) {
+        this.searchDocument = event.target.value;
+    }
+
+    handleClick() {
+        // Lógica de manipulação dos resultados da busca, se necessário
+        if (this.contacts.data && this.contacts.data.length > 0) {
+            console.log('Contatos encontrados:', this.contacts.data);
+        } else {
+            console.log('Nenhum contato encontrado');
+        }
+    }
+}
